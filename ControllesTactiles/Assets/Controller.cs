d7b6d2db810c8457c6e6de7ca0f565d2dff4f909
elvis1615 <elvis_rolando@hotmail.es>
2016-03-15 10:08:19 -0600

    fallo
    fallando los controles no se ajustan a la posicion

---
+++ ControllesTactiles/Assets/Controller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Controller : MonoBehaviour {
+    Rigidbody r;
+	// Use this for initialization
+	void Start () {
+        r = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float x=CrossPlatformInputManager.GetAxis("Horizontal");
+        float y=CrossPlatformInputManager.GetAxis("Vertical");
+        float x1 = CrossPlatformInputManager.GetAxis("Mouse X");
+        float y1 = CrossPlatformInputManager.GetAxis("Mouse Y");
+        r.velocity = new Vector3(x,y,0);
+        Quaternion a = Quaternion.Euler(0,0,Mathf.Atan2(y1,x1)*180/Mathf.PI);
+        transform.rotation = Quaternion.Slerp(r.rotation,a,10);
+    }
+}
---
+++ ControllesTactiles/Assets/Panel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour, IPointerUpHandler, IPointerDownHandler, IDragHandler
+{
+    public GameObject g;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        g.SetActive(true);
+        g.GetComponent<RectTransform>().position = data.position;
+        
+        g.GetComponent<Joystick>().OnPointerDown(data);
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        
+        
+        g.GetComponent<Joystick>().OnPointerDown(data);
+        g.SetActive(false);
+    }
+    
+    public void OnDrag(PointerEventData data) {
+        g.GetComponent<Joystick>().OnDrag(data);
+    }
+}
